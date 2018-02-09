96d6c2efd8d9bbf333643884bade2a8850e19ae2
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-02-09 09:29:16 -0500

    merge

--- Lab3.2/src/Merge.java
+++ Lab3.2/src/Merge.java
@@ -2,10 +2,36 @@
 public class Merge {
 	public static String[] merge(String[] list1, String[] list2) 
 	{
-		int mid1 = list1.length/2;
-		for(int i = 0; i < list1.length;i++)
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		int totalLength = list1.length + list2.length;
+		String[] mergeArray = new String[totalLength];
+		while(i < list1.length && j < list2.length)
 		{
-			
+			int value1 = (list1[i].compareTo(list2[j]));
+			if (value1 < 0)
+			{
+				String small = list1[i];
+				String big = list2[j];
+				mergeArray[k] = small;
+				mergeArray[k+1] = big;
+				i++;
+				j++;
+				k++;
+			}
+			else
+			{
+				String big = list1[i];
+				String small = list2[j];
+				mergeArray[k] = small;
+				mergeArray[k+1] = big;
+				i++;
+				j++;
+				k++;
+			}
 		}
+		return mergeArray;
+		
 	}
 }
