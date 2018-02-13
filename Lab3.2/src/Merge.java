f150bc41b83a12ca5cd34c6724d9f8d46e946726
nickn <nickn@DESKTOP-B2VPUKN>
2018-02-12 23:28:31 -0500

    eh

--- Lab3.2/src/Merge.java
+++ Lab3.2/src/Merge.java
@@ -2,36 +2,32 @@
 public class Merge {
 	public static String[] merge(String[] list1, String[] list2) 
 	{
+		//index for list 1
 		int i = 0;
+		//index for list 2
 		int j = 0;
+		//index for list 3
 		int k = 0;
 		int totalLength = list1.length + list2.length;
-		String[] mergeArray = new String[totalLength];
-		while(i < list1.length && j < list2.length)
+		
+		String[] mergeString = new String[totalLength];
+		while (i < list1.length && j <list2.length)
 		{
-			int value1 = (list1[i].compareTo(list2[j]));
-			if (value1 < 0)
-			{
-				String small = list1[i];
-				String big = list2[j];
-				mergeArray[k] = small;
-				mergeArray[k+1] = big;
-				i++;
-				j++;
-				k++;
-			}
+			int small = list1[i].compareTo(list2[j]);
+			if (small < 0)
+				{
+					mergeString[k] = list1[i];
+					i++;
+				}
+			
 			else
-			{
-				String big = list1[i];
-				String small = list2[j];
-				mergeArray[k] = small;
-				mergeArray[k+1] = big;
-				i++;
-				j++;
+				{
+					mergeString[k] = list2[j];
+					j++;
+				}
 				k++;
-			}
 		}
-		return mergeArray;
+		return mergeString;
 		
 	}
 }
--- Lab3.2/src/Runner.java
+++ Lab3.2/src/Runner.java
@@ -1,13 +1,15 @@
 
 public class Runner {
-	String [] test1 = {"apple","cucumber","microsoft","zorro"};
-	String [] test2 = {"banana","cherry","mahoghany","oreos","pinata"};
-	int[] test3 = {3, 4, 2, 7, 12 ,22 , 0, 1};
+	public static void main(String[] args) {
+		String [] test1 = {"apple","cucumber","microsoft","zorro"};
+		String [] test2 = {"banana","cherry","mahoghany","oreos","pinata"};
+		int[] test3 = {3, 4, 2, 7, 12 ,22 , 0, 1};
 	
-	long start = System.nanoTime();
-	String[] mergeResult = Merge.merge(test1,test2);
-	long end = System.nanoTime();
-	long time = end - start;
-	System.out.println("Merge test took: " + time + "nanoseconds");
-	System.out.println(Merge.)
+		long start = System.nanoTime();
+		String[] mergeResult = Merge.merge(test1,test2);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge test took: " + time + "nanoseconds");
+		System.out.println((mergeResult));
+	}
 }
