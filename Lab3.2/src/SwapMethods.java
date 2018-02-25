fb48b5cc253c6934271469223a61c5df01958c16
nickn <nickn@DESKTOP-B2VPUKN>
2018-02-25 15:39:19 -0500

    qf

---
+++ Lab3.2/src/SwapMethods.java
@@ -0,0 +1,9 @@
+
+public class SwapMethods {
+	public static void swap(int[] elements, int index1, int index2)
+	{
+		 int temp=elements[index1];
+		 elements[index1]= elements[index2];
+		 elements[index2]=temp;
+	}
+}
