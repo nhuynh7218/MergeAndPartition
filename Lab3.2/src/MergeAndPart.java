ae467e4acad6b0d589a18bd886e23710fd4df945
nickn <nickn@DESKTOP-B2VPUKN>
2018-03-04 22:42:08 -0500

    weg

--- Lab3.2/src/MergeAndPart.java
+++ Lab3.2/src/MergeAndPart.java
@@ -55,30 +55,30 @@ public class MergeAndPart {
 		
 		int length = list.length;
 		int countIndex = 0;
-<<<<<<< HEAD
-		int pivot = list[0];
+
+		
 		int left = 0;
-=======
+
 		int pivot = list[countIndex];
 		int back = list.length-1;
->>>>>>> branch 'master' of https://github.com/nhuynh7218/MergeAndPartition.git
+
 		for(int i = 1; i < length; i++)
 			{
 			//when less than pivot, swap
 			
 				if (list[i] <= pivot)
 				{
-<<<<<<< HEAD
+
 					SwapMethods.swap(list, list[i], pivot);
 					SwapMethods.swap(list, list[left+1], pivot);
 					i++;
 					left++;
 					countIndex++;
-=======
+
 					SwapMethods.swap(list, i, i-1);
 					//changes the index value of pivot to the place of the number swapped
 					countIndex = i;
->>>>>>> branch 'master' of https://github.com/nhuynh7218/MergeAndPartition.git
+
 				}
 				else if (list[i] > pivot && back > i)
 				{
